db4e24fc0932b652eb494afd00edae86be42b05f
Kolpak <164049849+Horven228@users.noreply.github.com>
2025-05-19 18:59:21 +0300

    Add files via upload

---
+++ blogicum/blog/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import Comment, Post, User
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('text',)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'pub_date', 'is_published', 'category', 'location', 'image']
+        
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name', 'last_name','username')
+
+        
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            'username',             
+            'password1', 
+            'password2',
+        )
+
+        
+
